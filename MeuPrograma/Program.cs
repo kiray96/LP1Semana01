eb202dbe22d3c7156e9cfd678c5149346d369c3b
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-19 23:30:37 +0000

    Add Documentation Comment

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -4,6 +4,10 @@ namespace MeuPrograma
 {
     public class Program
     {
+        /// <summary>
+        /// Prints "Hello LP1!" and "Bye world"
+        /// </summary>
+        /// <param name="args">program arguments</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
