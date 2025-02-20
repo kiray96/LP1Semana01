282c2a5fb5a9b37523374216b253148d0618231e
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-20 19:49:53 +0000

    create VariousTypes project that prints some integral types

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            short orange = 6;
+            int apple = 300 * orange;
+            int strawberry = 4 + apple;
+            long banana = 5L + 8L;
+
+            Console.WriteLine(orange);
+            Console.WriteLine(apple);
+            Console.WriteLine(strawberry);
+            Console.WriteLine(banana);
+        }
+    }
+}
