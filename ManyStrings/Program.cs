ed933445ed0d6e314615da713f22b134ac8fd57f
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-20 22:41:28 +0000

    Add concatenated and interpolated strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -13,6 +13,18 @@ namespace ManyStrings
             // Print strings
             Console.WriteLine(love);
             Console.WriteLine(love2);
+
+            //int for the strings
+            int eleven = 12;
+            // Define string using concatenation
+            string you = "you "; 
+            string love3 = you + "can " + "also " + "give " + "me " + "an " + eleven;
+            love3 += " maybe?";
+            string love4 = $"{you}can also give me an {eleven} maybe?";
+
+            // Print strings
+            Console.WriteLine(love3);
+            Console.WriteLine(love4);
         }
     }
 }
