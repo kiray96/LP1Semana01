45bc5d0e807c925f1eb01f5b419de6f60abb58f3
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-20 19:55:07 +0000

    Add comments to the program

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,11 +6,13 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
+            // create and assign integral number variables
             short orange = 6;
             int apple = 300 * orange;
             int strawberry = 4 + apple;
             long banana = 5L + 8L;
 
+            // Prints the integrals number variables
             Console.WriteLine(orange);
             Console.WriteLine(apple);
             Console.WriteLine(strawberry);
