41d256117564ebdbcce7ab9b239fc72b41330851
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-20 22:20:46 +0000

    Create the ManyStrings project and make and print strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Define strings
+            string love = "Please \t give \t me \t a \t 10 \t ! \t ! \t !"; 
+            string love2 = "Please, \t please \t give \t me \t a \t 10 \t \u2620 \t \u2620 \t \u2620 \t \u2620";
+
+            // Print strings
+            Console.WriteLine(love);
+            Console.WriteLine(love2);
+        }
+    }
+}
