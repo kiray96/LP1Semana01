70804812e6465c888e67852876f49bd7763e93e2
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-21 13:54:09 +0000

    Resolution of the seventh exercise

---
+++ AskForInput/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.XPath;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Ask the player to insert a integral number
+            Console.WriteLine("Please insert an integral number?");
+
+            // Read the number of the player
+            string number = Console.ReadLine();
+            // Ask the player to insert a integral namber
+            int numberint = int.Parse(number);
+
+
+            //Ask the player to insert a real number
+            Console.WriteLine("Please insert an real number?");
+
+            // Read the number of the player
+            string number2 = Console.ReadLine();
+            // Ask the player to insert a real namber
+            float numberreal = float.Parse(number2);
+
+            //Sum of the two numbers requested by the player
+            float result = numberint + numberreal;
+
+            //Print the result
+            Console.WriteLine("The result is: " + result);
+             
+        }
+    }
+}
